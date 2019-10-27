aa282b4fb73e04504cad46e8620d3f2054cc62ea
MJOlkmn <matematik@mail.ru>
2019-10-27 23:13:22 +0700

    Snake (Змейка) 7 lvl 27-10-2019

---
+++ 8.Games/src/com/javarush/games/snake/Snake.java
@@ -0,0 +1,33 @@
+package com.javarush.games.snake;
+
+import com.javarush.engine.cell.*;
+import com.sun.javafx.scene.traversal.Direction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Snake {
+    int x, y;
+    private static final String HEAD_SIGN = "\uD83D\uDC7E";
+    private static final String BODY_SIGN = "\u26AB";
+    private List<GameObject> snakeParts = new ArrayList<>();
+    public boolean isAlive = true;
+    private Direction direction = Direction.LEFT;
+
+    public Snake(int x, int y) {
+        snakeParts.add(new GameObject(x, y));
+        snakeParts.add(new GameObject(x + 1, y));
+        snakeParts.add(new GameObject(x + 2, y));
+    }
+
+    public void draw(Game game) {
+        game.setCellValueEx(snakeParts.get(0).x, snakeParts.get(0).y, Color.NONE, HEAD_SIGN, isAlive ? Color.BLACK : Color.RED, 75);
+        for (int i = 1; i < snakeParts.size(); i++) {
+            game.setCellValueEx(snakeParts.get(i).x, snakeParts.get(i).y, Color.NONE, BODY_SIGN, isAlive ? Color.BLACK : Color.RED, 75);
+        }
+    }
+
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+}
